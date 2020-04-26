41bd8dba9e58d20631152e044ac56550b16f71ae
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-04-26 10:56:08 +0700

    make validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,25 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/',(req, res, next)=>{
-  Product.find().exec().then(docs => {
-    console.log(docs);
-    res.status(200).json(docs);
+  Product.find()
+  .select('name price _id')
+  .exec()
+  .then(docs => {
+    const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+            return {
+                name: doc.name,
+                price: doc.price,
+                _id: doc._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://127.0.0.1:3000/products/' + doc._id
+                }
+            }
+        })
+    };
+    res.status(200).json(response);
   }).catch(err => {
       res.status(500).json({
           error: err
@@ -28,8 +44,17 @@ router.post('/',(req, res, next)=>{
     product.save().then(result => {
         console.log(result);
         res.status(201).json({
-            message: 'Post',
-            createdProduct: result
+            message: 'Created success',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'POST',
+                    url: 'http://127.0.0.1:3000/products/' + result._id
+                }
+
+            }
         });
     })
     .catch(err => {
@@ -43,10 +68,19 @@ router.post('/',(req, res, next)=>{
 
 router.get('/:productId', (req, res, next)=>{
     const id = req.params.productId;
-    Product.findById(id).exec().then(doc => {
+    Product.findById(id)
+    .select('name price _id')
+    .exec()
+    .then(doc => {
         console.log("From Database", doc);
         if (doc){
-            res.status(200).json(doc);
+            res.status(200).json({
+                product: doc,
+                request: {
+                    type: 'GET',
+                    url: 'http://127.0.0.1:3000/products/' + doc._id
+                }
+            });
         }else{
             res.status(404).json({
                 message:"ID not found"
@@ -69,6 +103,7 @@ router.get('/:productId', (req, res, next)=>{
     } */
 });
 
+
 /* How to update for this, you can write on body like here
 [
 	{"propName": "name", "value": "dalgondal"}
@@ -81,8 +116,13 @@ router.patch('/:productId', (req, res, next)=>{
         updateOps[ops.propName] = ops.value;
     }
     Product.update({ _id: id }, { $set: updateOps }).exec().then(result => {
-        console.log(result);
-        res.status(200).json(result);
+        res.status(200).json({
+            message: 'Product updated',
+            request: {
+                type: 'GET',
+                url: 'http://127.0.0.1:3000/products/' + id
+            }
+        });
     }).catch().catch(err => {
         res.status(500).json({
             error: err
@@ -90,12 +130,17 @@ router.patch('/:productId', (req, res, next)=>{
     });
 });
 
-
-
 router.delete('/:productId', (req, res, next)=>{
    let id = req.params.productId;
     Product.remove({ _id: id }).exec().then(result => {
-        res.status(200).json(result);
+        res.status(200).json({
+            message: 'Product deleted',
+            request: {
+                type: 'POST',
+                url: 'http://127.0.0.1:3000/products/' + id,
+                body: {name: 'String', price: 'Number'}
+            }
+        });
     })
     .catch(err => {
         console.log(err);
